6abd22786761d36467bdd7bf73fd2a20ae65af38
RSangers <rubensangers@hotmail.com>
2020-01-19 17:17:43 +0100

    installed ws

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -2,12 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/splash', function(req, res) {
+router.get('/splash.html', function(req, res) {
   res.sendFile('splash.html', { root: './public' });
 });
 
 /* Pressing the 'PLAY' button, returns this page */
-router.get("/game", function(req, res) {
+router.get("/game.html", function(req, res) {
   res.sendFile("game.html", { root: "./public" });
 });
 
