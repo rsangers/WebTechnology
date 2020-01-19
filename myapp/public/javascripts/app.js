0c5884af7b5a608628aa7f10b700ca3a552943d2
RSangers <rubensangers@hotmail.com>
2020-01-19 11:25:20 +0100

    slight changes to interactivity

--- myapp/public/javascripts/app.js
+++ myapp/public/javascripts/app.js
@@ -3,28 +3,30 @@ var main = function () {
     
     //Create column and tile elements
     for(let i=0;i<7;i++){
-        var $newColumn=$("<div>");
-        $newColumn.id="c"+i;
+        var $newColumn=$("<div id=c"+i+">");
         $newColumn.addClass("column");
         for(let j=0;j<6;j++){
-            var $newTile=$("<div>");
-            $newTile.id="t"+i*6+j;
-            console.log($('#'+$newTile.id));
+            var $newTile=$("<div id=t"+i*6+j+">");
+            //$newTile.isFree=true;
             $newTile.addClass("tile");
             $newColumn.append($newTile);
+            $("#t"+j).attr("isFree",true);
+            console.log($("#t"+j).attr("isFree"));
         }
         $(".board").append($newColumn);
-
     }
 
     //Add column interactivity
     $(".column").click(function() {
-        $(this).find(".tile","background-color:gray").last().css("background-color", "red");
-        console.log($(this).find(".tile").first().attr("background-color"));
+        //$(this).find(".tile").filter(function(){
+          //  return $(this).isFree;
+        //}).last().css("background-color", "red");
+        console.log($(this).find(".tile").last().isFree);
+        //console.log($(this).find(".tile").first().attr("background-color"));
     });
 
     /*
-    $(".comment-input button").on("click", function (event) {
+    $("div").on("click", function (event) {
       var $new_comment = $("<p>"),
       comment_text = $(".comment-input input").val();
       $new_comment.text(comment_text);
