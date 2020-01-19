28a4033dbf50acbb0c654f90eeaa78ae8c2b39e4
RSangers <rubensangers@hotmail.com>
2020-01-19 16:59:01 +0100

    test server

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/splash', function(req, res) {
+  res.sendFile('splash.html', { root: './public' });
+});
+
+/* Pressing the 'PLAY' button, returns this page */
+router.get("/game", function(req, res) {
+  res.sendFile("game.html", { root: "./public" });
 });
 
 module.exports = router;
